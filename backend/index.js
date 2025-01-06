57f08476ef7e896efd6b5eb0f6b1bfb3f727fe95
asstroboyz <gandagunawan36@gmail.com>
2025-01-07 02:19:47 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ try {
 } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin: 'http://localhost:3121'}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
